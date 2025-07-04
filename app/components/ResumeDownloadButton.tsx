4c08b0c45115252ad162f0429a87c8a23ea74bf2
nickmafforti <nickmafforti@gmail.com>
2025-07-04 19:03:23 +0000

    fix resume download link

--- app/components/ResumeDownloadButton.tsx
+++ app/components/ResumeDownloadButton.tsx
@@ -7,7 +7,7 @@ export default function ResumeDownloadButton() {
     return (
         <>
         <Box mt={3}>
-          <Button variant="contained" color="primary" href="portfolio/resume.pdf" target="_blank" >
+          <Button variant="contained" color="primary" href="resume.pdf" target="_blank" >
             Download Resume
           </Button>
         </Box>
